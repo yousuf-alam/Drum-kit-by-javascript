6ba3f308fd9706a7b6b8d55c1bcf44b8195dd3aa
Yousuf Alam <39653613+yousuf-alam@users.noreply.github.com>
2021-10-14 13:06:12 +0600

    Add files via upload

---
+++ Drum/index.js
@@ -0,0 +1,64 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+
+      case "w":
+
+        var crash = new Audio('sound/crash.mp3');
+        crash.play();
+        break;
+
+      case "a":
+
+        var kick = new Audio('sound/kick-bass.mp3');
+        kick.play();
+        break;
+
+      case "s":
+
+        var snare = new Audio('sound/snare.mp3');
+        snare.play();
+        break;
+
+      case "d":
+
+        var tom1 = new Audio('sound/tom-1.mp3');
+        tom1.play();
+        break;
+
+      case "j":
+
+        var tom2 = new Audio('sound/tom-2.mp3');
+        tom2.play();
+        break;
+
+      case "k":
+
+        var tom3 = new Audio('sound/tom-3.mp3');
+        tom3.play();
+        break;
+
+      case "l":
+
+        var tom4 = new Audio('sound/tom-4.mp3');
+        tom4.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+
+
+    }
+
+
+  });
+
+}
